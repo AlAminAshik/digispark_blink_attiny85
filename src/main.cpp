07b8dddc03dbed2611aaf798f433a931be2eb675
AlAminAshik <106368872+AlAminAshik@users.noreply.github.com>
2025-01-10 18:04:06 +0600

    upload using platformio

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 //so initiate upload process and the serial monitor will ask to plugin the device
 //then insert the device and the code will be uploaded to the board
 //if you program the board externally using arduino as ISP programmer, this method will not work anymore
+//"upload_protocol = micronucleus" this line is added to initiate the upload process in platformio.ini file
+
 #include <Arduino.h>
 // the setup function runs once when you press reset or power the board
 void setup() {
