bd79a28432bb993eb24e783fc1995d7d171534d8
AlAminAshik <106368872+AlAminAshik@users.noreply.github.com>
2025-01-10 17:48:27 +0600

    comment for usage added

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,8 @@
+//This code shows how to upload simple blink code to digispark attiny85 board
+//digispark board only stays at program mode for 5seconds after plugged in to computer
+//so initiate upload process and the serial monitor will ask to plugin the device
+//then insert the device and the code will be uploaded to the board
+//if you program the board externally using arduino as ISP programmer, this method will not work anymore
 #include <Arduino.h>
 // the setup function runs once when you press reset or power the board
 void setup() {
